a6842cdee63a81c408465b70d6a6e7ca2ee65992
George Coker <cgeorge@axamansard.com>
2021-07-21 14:46:30 +0100

    first commit

---
+++ TaskList.Domain/TaskItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Domain
+{
+    public class TaskItem : BaseEntity
+    {
+        [Required]
+        [StringLength(255)]
+        [Display(Name = "TAsk Name")]
+        public string TaskName { get; set; }
+
+        [Display(Name = "Is Completed?")]
+        public bool IsCompleted { get; set; } = false;
+    }
+}
